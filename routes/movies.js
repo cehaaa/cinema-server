9bcf38fee79a464ef1f721c4d983f0caaa1ad041
christianngouw <christiangouw168@gmail.com>
2021-09-19 00:00:35 +0700

    feat: add query paramter `is_now_showing` for `/movies` endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -7,10 +7,21 @@ const isBestMovies = (movie) => {
   return movie.is_best_movie === true;
 };
 
+const isNowShowing = (movie) => {
+  return movie.is_now_showing === true;
+};
+
 router.get("/", (req, res) => {
   if (req.query.is_best_movies) {
+    // * check, if movies is the best movie
+
     let bestMovies = movies.filter(isBestMovies);
     res.send(bestMovies);
+  } else if (req.query.is_now_showing) {
+    // * check, if movies is on show
+
+    let nowShowing = movies.filter(isNowShowing);
+    res.send(nowShowing);
   } else res.send(movies);
 });
 
