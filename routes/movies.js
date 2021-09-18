b59b2a7c990c75077bf0246e788700e8879280fa
christianngouw <christiangouw168@gmail.com>
2021-09-18 23:27:12 +0700

    feat: add query parameter `is_best_movies`  for `/movies` endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -3,8 +3,15 @@ const movies = require("../@fake-db/movies.json");
 const express = require("express");
 const router = express.Router();
 
+const isBestMovies = (movie) => {
+  return movie.is_best_movie === true;
+};
+
 router.get("/", (req, res) => {
-  res.json(movies);
+  if (req.query.is_best_movies) {
+    let bestMovies = movies.filter(isBestMovies);
+    res.send(bestMovies);
+  } else res.send(movies);
 });
 
 module.exports = router;
