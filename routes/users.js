8b6225ab69bcc4fdb066fee04afd7997d4e6ff5b
christianngouw <christiangouw168@gmail.com>
2021-09-19 18:45:05 +0700

    feat: add query parameter `is_friend` and `is_suggested_friend` for `/users` endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
-const userData = require("../@fake-db/users.json");
+const users = require("../@fake-db/users.json");
 
 const express = require("express");
 const router = express.Router();
 
+const isFriend = (friend) => {
+  return friend.is_friend === true;
+};
+
+const isSuggestedFriend = (users) => {
+  return users.is_friend === false;
+};
+
 router.get("/", (req, res) => {
-  res.send(userData);
+  if (req.query.is_friend) {
+    let friends = users.filter(isFriend);
+    res.send(friends);
+  } else if (req.query.is_suggested_friend) {
+    let suggestedFriends = users.filter(isSuggestedFriend);
+    res.send(suggestedFriends);
+  } else res.send(users);
 });
 
 module.exports = router;
